57518e9c70486330e7c6c614e9c9ee3ad0c802ab
blekiki8-eng <blekiki8@gmail.com>
2026-04-25 23:07:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,9 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const TOKEN = process.env.BOT_TOKEN;
+const TOKEN = "8672090410:AAFpNMdSXJ3NNli4BNLa5sdQ1R4qUN-TzDQ";
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-const WEBAPP_URL = "https://YOUR-FRONTEND-LINK";
-
-// /start — ВАЖЛИВО
 bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-
-  bot.sendMessage(chatId, "🐟 Welcome to Fish Cash!", {
-    reply_markup: {
-      keyboard: [
-        [
-          {
-            text: "🎮 Open Fish Cash",
-            web_app: { url: WEBAPP_URL }
-          }
-        ]
-      ],
-      resize_keyboard: true
-    }
-  });
-});
-
-// тест
-bot.on("message", (msg) => {
-  if (msg.text && msg.text.toLowerCase() === "hi") {
-    bot.sendMessage(msg.chat.id, "Bot is alive ✅");
-  }
+  bot.sendMessage(msg.chat.id, "🐟 Bot is working!");
 });
